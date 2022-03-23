294b6e0618f3733482c3c1d4bd16eb89d17b14c2
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2022-03-23 08:51:47 +0200

    testing heroku on the home page

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
 
-include('../config/dbconnect.php');
+include('./config/dbconnect.php');
 if (isset($_POST['submit'])) {
 
     if ($_FILES["image"]["error"] == 4) {
@@ -70,9 +70,11 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- <link rel="stylesheet" href="../styles/main.css"> -->
-    <link rel="stylesheet" href="../styles/admin.css">
-    <link rel="stylesheet" href="../styles/footer.css">
-    <link rel="stylesheet" href="../styles/home.css">
+    <link rel="stylesheet" href="./styles/admin.css">
+    <link rel="stylesheet" href="./styles/footer.css">
+    <link rel="stylesheet" href="./styles/home.css">
+    <link rel="stylesheet" href="./styles/responsive.css">
+    <link rel="stylesheet" href="./styles/homeres.css">
     <!-- <link href="https://unpkg.com/tailwindcss@^2.2.7/dist/tailwind.min.css" rel="stylesheet">
 <script src="https://cdn-tailwindcss.vercel.app/"></script> -->
 
@@ -80,23 +82,29 @@ if (isset($_POST['submit'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
+
+
 <header>
-    <nav>
-        <div class="logo">
-            <h4>X-MEN</h4>
-        </div>
+        <div class="overlay-header">
+        <nav class="nav- ">
+            <div class="logo-del"><h1>X-MEN</h1></div>
+            <div class="openMenu"><img src="./assets/icons/humberger.png" alt=""></div>
+            <ul class="mainMenu">
+                <li><a href="./index.html">Home</a></li>
+                <li><a href="./pages/dashboard.html">Blog</a></li>
+             
+               
+                <div class="closeMenu"><img src="./assets/icons/close.png" alt=""></div>
+            </ul>
+        </nav>
+    </div>
+    
 
 
-        <ul>
-            <li><a href="logout.php"> Home</li></a>
-            <li><a href="logout.php"> Contact us</li></a>
-            <li><a href="logout.php"> Login</li></a>
 
-        </ul>
+       
 
-
-    </nav>
-</header>
+    </header>
 
 
 
@@ -326,11 +334,8 @@ if (isset($_POST['submit'])) {
 
     <!-- ====================actual body content============== -->
 
-    <div class="section-hero">
         <h3 class="text-center">Heroes</h3>
-        <div class="addbutton">
         
-        </div>
         
 
         <!-- ====================================fetching =====================-->
@@ -405,7 +410,7 @@ if (isset($_POST['submit'])) {
 
 
         </div>
-
+  
 
         
   
@@ -474,7 +479,7 @@ if (isset($_POST['submit'])) {
         <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap4.min.js"></script>
         <script src="../script/dataTables.js"></script>
-
+        <script src="../script/burger.js"></script>
         <script src="../script/more.js"></script>
         <script src="../script/deleteModal.js"></script>
 
